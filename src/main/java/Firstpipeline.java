9f5294cdc4b02cb91d1963d9e405a7ed6c8e240a
SaritGH <you@example.com>
2020-11-20 03:12:14 -0500

    adding third statement

--- src/main/java/Firstpipeline.java
+++ src/main/java/Firstpipeline.java
@@ -2,5 +2,6 @@ public class Firstpipeline {
     public static void main(String[] args) {
         System.out.println("Hello this is my first pipeline :)");
         System.out.println("Adding something new ");
+        System.out.println("Trying to pass build 6 :) :)");
     }
 }
