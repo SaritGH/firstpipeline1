e86a8e34119e76dd22bdc1a189fddc2a6a2bfcd1
SaritGH <you@example.com>
2020-11-20 02:13:11 -0500

    Adding something new

--- src/main/java/Firstpipeline.java
+++ src/main/java/Firstpipeline.java
@@ -1,5 +1,6 @@
 public class Firstpipeline {
     public static void main(String[] args) {
         System.out.println("Hello this is my first pipeline :)");
+        System.out.println("Adding something new ");
     }
 }
